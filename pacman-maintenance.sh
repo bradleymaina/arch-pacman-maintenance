bbecd8bcf3d00558c6d96cad66ccbb0682b04d45
bradleymaina <bradleymaina@proton.me>
2026-09-17 13:14:51 +0300

    feat: Fetch live mirrors and update mirror list

--- pacman-maintenance.sh
+++ pacman-maintenance.sh
@@ -1,12 +1,47 @@
 #!/usr/bin/env bash
 
 check_network(){
-    ping -c 1 google.com
+    ping -q -c 1 google.com
     status=$?
     if [ "$status" -ne 0 ]; then
         echo "Network is down. Please  check your internet connection."
         return 1
+    else 
+        echo "Network is up. Proceeding with mirrorlist update........"
     fi
 }
 
-check_network
+update_mirrorlist(){
+   # safely backup exisisting mirrorlist
+   sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup || {
+        echo "Failed to backup mirrorlist."
+        return 1
+   }
+
+   # fetch live mirrorlist, filter and sort by speed and update the mirrorlist
+   curl  "https://archlinux.org/mirrorlist/?country=all&protocol=https&use_mirror_status=on" \
+   | sed -e 's/^#Server/Server/' -e '/^#/d' > /tmp/mirrorlist.new 
+
+   if [ "$?" -ne 0 ]; then
+        echo "Failed to fetch mirrorlist."
+        return 1
+   fi
+
+   rankmirrors -m 10 -w -n 6 /tmp/mirrorlist.new > /tmp/mirrorlist.ranked
+
+   if [ "$?"  -ne 0 ]; then
+        echo "Failed to rank mirrrors."
+        return 1
+    fi
+
+    cp /tmp/mirrorlist.ranked /etc/pacman.d/mirrorlist || {
+        echo "Failed to update mirrorlist."
+        return 1 
+        }
+
+    echo "Mirrorlist updated successfully."
+    return 0
+}
+
+check_network || exit 1
+update_mirrorlist || exit 1 
