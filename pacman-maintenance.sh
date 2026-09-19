47797c99231bee5dd934863960d6c5378e6b40f4
bradleymaina <bradleymaina@proton.me>
2026-09-20 01:28:28 +0300

    feat: Log all changes made and save them to /var/log/pacman-maintenance.log

--- pacman-maintenance.sh
+++ pacman-maintenance.sh
@@ -3,8 +3,17 @@ set -o pipefail
 
 exec 9>/tmp/pacman-maintenance.lock
 
+log(){
+    severity="$1"
+    message="$2"
+
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [$severity] $message" >> /var/log/pacman-maintenance.log
+
+}
+
+
 flock -n 9 || {
-    echo "Another instance of the script is already running."
+    log "ERROR" "Another instance of the script is already running."
     exit 1
 }
 
@@ -12,17 +21,18 @@ check_network(){
     ping -q -c 1 google.com
     status=$?
     if [ "$status" -ne 0 ]; then
-        echo "Network is down. Please  check your internet connection."
+        log "ERROR" "Network is down. Please check your internet connection."
         return 1
     else 
-        echo "Network is up. Proceeding with mirrorlist update........"
+        log "INFO" "Network is up. Proceeding with mirrorlist update........"
+        return 0
     fi
 }
 
 update_mirrorlist(){
    # safely backup exisisting mirrorlist
    sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup || {
-        echo "Failed to backup mirrorlist."
+        log "ERROR" "Failed to backup mirrorlist."
         return 1
    }
 
@@ -32,33 +42,38 @@ update_mirrorlist(){
    |jq -r '.urls[] | select(.active and .completion_pct == 1.0 and .protocol == "https") | "Server = \(.url)$repo/os/$arch"' > /tmp/mirrorlist.new 
 
    if [ "$?" -ne 0 ]; then
-        echo "Failed to fetch mirrorlist."
+        log "ERROR" "Failed to fetch mirrorlist."
         return 1
    fi
 
    rankmirrors -m 10 -w -n 6 -p /tmp/mirrorlist.new > /tmp/mirrorlist.ranked
 
    if [ "$?"  -ne 0 ]; then
-        echo "Failed to rank mirrrors."
+        log "ERROR" "Failed to rank mirrors."
         return 1
     fi
 
     cp /tmp/mirrorlist.ranked /etc/pacman.d/mirrorlist || {
-        echo "Failed to update mirrorlist."
+        log "ERROR" "Failed to update mirrorlist."
         return 1 
         }
 
-    echo "Mirrorlist updated successfully."
+    log "INFO" "Mirrorlist updated successfully."
     return 0
 }
 
 upgrade_system(){
-    sudo pacman -Syu --noconfirm || {
-        echo "System upgrade failed."
-        return 1
-    }
+    sudo pacman -Syu --noconfirm
 
+    if [ "$?" -eq 0 ]; then
+        log "INFO" "System upgraded successfully."
+        return 0
+    else
+        log "ERROR" "System upgrade failed."
+        return 1
+    fi
 }
+
 
 check_network || exit 1
 update_mirrorlist || exit 1 
